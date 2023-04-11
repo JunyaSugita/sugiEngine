94712b961031a8cc0863cc0720dfb37875f6a2b8
チャン スギ <junjunchan64@gmail.com>
2023-04-12 01:44:53 +0900

    消えたデータの復元

---
+++ SugiFramework.cpp
@@ -0,0 +1,71 @@
+#include "SugiFramework.h"
+
+void SugiFramework::Initialize()
+{
+	winApp = make_unique<WinApp>();
+	dxCom = make_unique<DXCommon>();
+	input = make_unique<Input>();
+	matrix4 = make_unique <Matrix4>();
+
+#pragma region windowsAPI初期化処理
+	winApp->CreateWindowScreen();
+	//コンソールへ文字入力
+	OutputDebugStringA("Hello DirectX\n");
+	OutputDebugStringA("This is sugiEngine\n");
+#pragma endregion
+
+#pragma region DirectX初期化処理
+	// DirectX初期化処理 ここから
+	dxCom->Initialize(winApp.get());
+	//キーボード入力の初期化
+	input->Initialize(winApp.get());
+#pragma endregion
+
+#pragma region 描画初期化処理
+	Object3d::StaticInitialize(dxCom->GetDevice());
+	Sprite::StaticInitialize(dxCom->GetDevice());
+	LightGroup::StaticInitialize(dxCom->GetDevice());
+#pragma endregion
+}
+
+void SugiFramework::Finalize()
+{
+#pragma region WindowsAPI後始末
+	winApp->DeleteWindow();
+#pragma endregion
+}
+
+void SugiFramework::Update()
+{
+	//キーボード入力
+	input->Update();
+}
+
+void SugiFramework::Run()
+{
+	//ゲーム初期化
+	Initialize();
+
+	while (true)
+	{
+		//毎フレーム更新
+		Update();
+		//終了リクエストを取得
+		if (GetIsEnd()) {
+			break;
+		}
+		//描画
+		Draw();
+	}
+
+	//ゲームの終了
+	Finalize();
+}
+
+bool SugiFramework::GetIsEnd()
+{
+	if (winApp->ProcMessage()) {
+		return true;
+	}
+	return false;
+}
---
+++ SugiFramework.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <Windows.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#include <vector>
+#include <string>
+#include <memory>
+#include <DirectXMath.h>
+#include <d3dcompiler.h>
+#include <DirectXTex.h>
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Matrix4.h"
+#include "WorldTransform.h"
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+#include "GrovalSetting.h"
+#include "WinApp.h"
+#include "DXCommon.h"
+#include"Input.h"
+#include "Object3d.h"
+#include "Sprite.h"
+#include "Model.h"
+#include "GameManager.h"
+#include "LightGroup.h"
+#include "ImGuiManager.h"
+
+class SugiFramework {
+public:
+	virtual ~SugiFramework() = default;
+
+	virtual void Initialize();
+
+	virtual void Finalize();
+
+	virtual void Update();
+
+	virtual void Draw() = 0;
+
+	void Run();
+
+	bool GetIsEnd();
+
+protected:
+	std::unique_ptr<WinApp> winApp = nullptr;
+	std::unique_ptr<DXCommon> dxCom = nullptr;
+	std::unique_ptr<Input> input = nullptr;
+	std::unique_ptr <Matrix4> matrix4 = nullptr;
+};
