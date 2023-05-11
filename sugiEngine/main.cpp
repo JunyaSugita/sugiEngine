181a4619611bd4432427a4d93934bf2de1d59dc3
JunyaSugita <94422859+JunyaSugita@users.noreply.github.com>
2023-05-11 13:12:20 +0900

    Revert "FBX_SDK導入(Release出来ない)"

--- sugiEngine/main.cpp
+++ sugiEngine/main.cpp
@@ -1,11 +1,8 @@
 ﻿#include "SugiFramework.h"
 #include "MyGame.h"
-//#include <fbxsdk.h>
 
 //windowsのエントリーポイント
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	//FbxManager* fbxManager = FbxManager::Create();
-
 	SugiFramework* game = new MyGame();
 
 	game->Run();
