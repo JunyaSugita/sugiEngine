48c64486b2cb15c2d16c05698baa6d434de8c2d2
JunyaSugita <94422859+JunyaSugita@users.noreply.github.com>
2023-06-13 13:08:44 +0900

    途中

--- sugiEngine/engine/2d/PostEffect.h
+++ sugiEngine/engine/2d/PostEffect.h
@@ -8,9 +8,17 @@ public:
 
 	void Initialize();
 	void Draw(ID3D12GraphicsCommandList* cmdList);
+	void PreDrawScene(ID3D12GraphicsCommandList* cmdList);
+	void PostDrawScene(ID3D12GraphicsCommandList* cmdList);
+
+public:
+	static const float clearColor[4];
 
 private:
 	ComPtr<ID3D12Resource> texBuff;
 	ComPtr<ID3D12DescriptorHeap>descHeapSRV;
+	ComPtr<ID3D12Resource> depthBuff;
+	ComPtr<ID3D12DescriptorHeap> descHeapRTV;
+	ComPtr<ID3D12DescriptorHeap> descHeapDSV;
 };
 
